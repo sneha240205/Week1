840d567f87cd7952a3c4b26f823bd3f3d58b85ea
sneha240205 <9923103110@mail.jiit.ac.in>
2025-07-30 11:27:54 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,21 @@
-#include<iostream>
-using namespace std;
+#include <stdio.h>
 
-int multiplication(int a,int b)
-{
-    return a*b;
-}
-int main()
-{
-    int a;
-    cin>>a;
-    int b;
-    cin>>b;
-    cout<<mutiplication(a,b);
+int main() {
+    int num, reversed = 0;
+
+    printf("Enter a 3-digit number: ");
+    scanf("%d", &num);
+
+    if(num < 100 || num > 999) {
+        printf("Invalid input. Please enter a 3-digit number.\n");
+        return 1;
+    }
+
+    while(num != 0) {
+        reversed = reversed * 10 + num % 10;
+        num /= 10;
+    }
+
+    printf("Reversed number: %d\n", reversed);
     return 0;
 }
