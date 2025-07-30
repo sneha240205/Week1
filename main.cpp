9c5dc693fde4d74c09513e25ef02c2bcee594fe9
sneha240205 <9923103110@mail.jiit.ac.in>
2025-07-30 10:42:29 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int multiplication(int a,int b)
+{
+    return a*b;
+}
+int main()
+{
+    int a;
+    cin>>a;
+    int b;
+    cin>>b;
+    cout<<mutiplication(a,b);
+    return 0;
+}
